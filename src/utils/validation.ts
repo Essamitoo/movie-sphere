04d52f5b2629ee450e8d067e74d31df2ce873cb7
Esteban Rodriguez <essamzilong@gmail.com>
2025-03-31 11:27:15 -0500

    Mejora de validaciones

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,17 +1,18 @@
-export const isValid = (validation: string, value: string) => {
-  if (validation === 'email') {
-     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-  }
+export const isValid = (validation: string, value: string, data?: { password?: string }) => {
+  switch (validation) {
+     case 'email':
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 
-  if (validation === 'password') {
-     return value.length > 5;
-  }
+     case 'password':
+        return /^(?=.*[A-Z])(?=.*\d).{6,}$/.test(value); // Mínimo 6 caracteres, 1 mayúscula, 1 número
 
-  if (validation === 'name') {
-     return value.length > 2;
-  }
+     case 'name':
+        return /^[a-zA-Z\s]+$/.test(value) && value.length > 2; // Solo letras y espacios, mínimo 3 caracteres
 
-  if (validation === 'repeatPassword') {
-     return value.length > 5;
+     case 'repeatPassword':
+        return value.length > 5 && value === data?.password; // Mínimo 6 caracteres y debe coincidir con `password`
+
+     default:
+        return false;
   }
 };
