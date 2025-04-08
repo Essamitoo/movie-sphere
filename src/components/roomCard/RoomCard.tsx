cc0962317964e33b1b8525ca72b8c6b02f2d4f3b
Frantorz <francobbel@hotmail.com>
2025-04-08 16:41:54 -0300

    Sala de chat

--- src/components/roomCard/RoomCard.tsx
+++ src/components/roomCard/RoomCard.tsx
@@ -2,43 +2,38 @@
 import Link from 'next/link'
 import { FaPlay } from 'react-icons/fa'
 import { RoomsCard } from '@/interfaces/IRooms'
-
-
-const RoomCard:React.FC<{room:RoomsCard}>= ({ room }) => {
-
-
-    return (
-        <div
-            className={`rounded-xl border h-[350px] m-4 p-1 flex flex-col items-center hover:scale-102 hover:cursor-pointer hover:border-[#00A878] w-[240px] border-[#00A878] bg-[#00A878]/10`}
-        >
-            <Link href={`/room/${room.username}`} className='rounded-xl h-[240px] w-full'>
-                <img
-                    src={room.url_media}
-                    className='rounded-xl h-[240px] w-full'
-                    alt={room.url_media}
-                />
-            </Link>
-            <div className='absolute m-[100px] w-20 h-20 rounded-full border-4 border-gray-300/70 flex justify-center items-center opacity-0 hover:opacity-80 transition duration-200 '>
-                <p>
-                    <FaPlay size={40} className='text-tertiary' />
-                </p>
-            </div>
-            <p className='text-[#00A878]'>{room.room}</p>
-            {false?(
-                    <button
-                        className='border border-[#00A878]/50 text-[#00A878] w-full rounded-sm hover:cursor-pointer bg-[#171717] h-[30px]'
-                    >
-                        Agregada
-                    </button>
-                ) : (
-                    <button
-                        className='border border-slate-100/50 w-full rounded-xl text-ms hover:cursor-pointer bg-[#171717] '
-                    >
-                        Entrar a la sala
-                    </button>
-                )}
-        </div>
-    )
+interface Props {
+	room: RoomsCard;
+	onEnterRoom: (roomName: string) => void;
 }
+
+const RoomCard: React.FC<Props> = ({ room, onEnterRoom }) => {
+	return (
+		<div
+			className={`rounded-xl border h-[350px] m-4 p-1 flex flex-col items-center hover:scale-102 hover:cursor-pointer hover:border-[#00A878] w-[240px] border-[#00A878] bg-[#00A878]/10`}
+		>
+			<Link href={`/room/${room.username}`} className='rounded-xl h-[240px] w-full'>
+				<img
+					src={room.url_media}
+					className='rounded-xl h-[240px] w-full'
+					alt={room.url_media}
+				/>
+			</Link>
+			<div className='absolute m-[100px] w-20 h-20 rounded-full border-4 border-gray-300/70 flex justify-center items-center opacity-0 hover:opacity-80 transition duration-200 '>
+				<p>
+					<FaPlay size={40} className='text-tertiary' />
+				</p>
+			</div>
+			<p className='text-[#00A878]'>{room.room}</p>
+
+			<button
+				onClick={() => onEnterRoom(room.room)}
+				className='border border-slate-100/50 w-full rounded-xl text-ms hover:cursor-pointer bg-[#171717]'
+			>
+				Entrar a la sala
+			</button>
+		</div>
+	);
+};
 
 export default RoomCard;
