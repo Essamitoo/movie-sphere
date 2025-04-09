c062d218f7595002c2bceb9338199b2241960d37
Angel <60985614+j14i@users.noreply.github.com>
2025-04-08 22:25:32 -0400

    Corregir la carga del avatar a cloudinary y agregar el token de acceso al localStorage

--- src/components/upload/UploadImageClient.tsx
+++ src/components/upload/UploadImageClient.tsx
@@ -1,102 +1,116 @@
-"use client"
+'use client'
 
-import { useState, useContext } from "react"
-import { AuthContext } from "@/contexts/authContext"
-import { Pencil } from "lucide-react"
-import Image from "next/image"
-import { updateUserAvatarService } from "@/services/authServices"
+import { useState, useContext } from 'react'
+import { AuthContext } from '@/contexts/authContext'
+import { Pencil } from 'lucide-react'
+import Image from 'next/image'
+import { updateUserAvatarService } from '@/services/authServices'
 interface UploadImageClientProps {
-  image?: string
+	image?: string
 }
 
 export default function UploadImageClient({ image }: UploadImageClientProps) {
-  const [imageUrl, setImageUrl] = useState(image)
-  const { user, setUser } = useContext(AuthContext)
-  const [deleteToken, setDeleteToken] = useState(user?.avatar_token || "")
+	const [imageUrl, setImageUrl] = useState(image)
+	const { user, setUser } = useContext(AuthContext)
+	const [deleteToken, setDeleteToken] = useState(user?.avatar_token || '')
 
-  if (!user) return null
+	if (!user) return null
 
-  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
-    if (!file) return
+	async function updateAvatar(
+		id: string,
+		avatarUri: string,
+		avatarToken: string
+	): Promise<void> {
+		try {
+			const res = await fetch(
+				process.env.NEXT_PUBLIC_API_URL + `v1/users/${id}`,
+				{
+					method: 'PATCH',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${user?.token}`,
+					},
+					body: JSON.stringify({
+						avatar: avatarUri,
+						avatar_token: avatarToken,
+					}),
+				}
+			)
 
-    if (deleteToken) {
-      await fetch(
-        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/delete_by_token`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ token: deleteToken }),
-        }
-      )
-      console.log("Imagen anterior eliminada")
-    }
+			const data = await res.json()
 
-    const formData = new FormData()
-    formData.append("file", file)
-    formData.append(
-      "upload_preset",
-      process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string
-    )
+			if (user?.id && data) {
+				const newAvatar = {
+					...user,
+					image: data.avatar,
+					avatar_token: data.avatar_token,
+				}
 
-    const res = await fetch(
-      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    )
+				setImageUrl(data.secure_url)
+				setDeleteToken(data.delete_token)
+				setUser(newAvatar)
+				localStorage.setItem('user', JSON.stringify(newAvatar))
+			}
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-    const data = await res.json()
-    setImageUrl(data.secure_url)
-    setDeleteToken(data.delete_token)
+	async function handleUserImage(e: React.ChangeEvent<HTMLInputElement>) {
+		e.preventDefault()
+		const file = e.target.files?.[0]
+		if (!file) return console.log('No se ha seleccionado un archivo')
 
-    if (user?.id) {
-      const updated = await updateUserAvatarService(
-        user.id,
-        data.secure_url,
-        data.delete_token
-      )
+		const payload = new FormData()
+		payload.append('file', file)
+		payload.append(
+			'upload_preset',
+			process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string
+		)
 
-      if (updated) {
-        const updatedUser = {
-            ...user,
-            image: updated.avatar, 
-            avatar_token: updated.avatar_token, 
-  
-        }
-        setUser(updatedUser)
-        localStorage.setItem("user", JSON.stringify(updatedUser))
-      }
-    }
-  }
+		// Cloudinary
+		try {
+			const res = await fetch(
+				`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+				{
+					method: 'POST',
+					body: payload,
+				}
+			)
 
-  return (
-    <div className="relative mx-auto size-30">
-      <div className="relative w-full h-full rounded-full overflow-hidden">
-        <Image
-          src={imageUrl || user?.image}
-          alt="user"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
+			const data = await res.json()
+			if (!user) return console.log('Usuario no encontrado')
+			updateAvatar(user?.id, data.secure_url, data.delete_token)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-      <label
-        htmlFor="file-upload"
-        className="absolute -bottom-0 -right-0 bg-white p-2 rounded-full hover:cursor-pointer hover:scale-105 transition duration-200 ease-in-out"
-      >
-        <Pencil className="w-4 h-4 text-gray-700" />
-      </label>
+	return (
+		<div className='relative mx-auto size-30'>
+			<div className='relative w-full h-full rounded-full overflow-hidden'>
+				<Image
+					src={imageUrl || user?.image}
+					alt='user'
+					fill
+					style={{ objectFit: 'cover' }}
+				/>
+			</div>
 
-      <input
-        id="file-upload"
-        type="file"
-        onChange={handleFileChange}
-        className="hidden"
-      />
-    </div>
-  )
+			<label
+				htmlFor='file-upload'
+				className='absolute -bottom-0 -right-0 bg-white p-2 rounded-full hover:cursor-pointer hover:scale-105 transition duration-200 ease-in-out'
+			>
+				<Pencil className='w-4 h-4 text-gray-700' />
+			</label>
+
+			<input
+				id='file-upload'
+				type='file'
+				onChange={(e) => handleUserImage(e)}
+				className='hidden'
+				accept='image/*'
+			/>
+		</div>
+	)
 }
