c152b412336cffea17a8d1fd4826be670435d057
Esteban Rodriguez <essamzilong@gmail.com>
2025-04-08 19:01:25 -0500

    urgente

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -76,7 +76,7 @@ const AuthProvider = ({ children }: ChildrenType) => {
 				name: data.name,
 				email: data.email,
 				password: '', // Sin password para login con Google
-				image: data.image,
+				image: data.image ||'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg',
 				account: 'free',
 				role: 'user',
 				favorites: [],
